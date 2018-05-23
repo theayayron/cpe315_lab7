c0cfa92d88b5437b27685e7ea2bc09383f979477
theayayron <aaronkawahara@gmail.com>
2018-05-23 13:46:45 -0700

    forgot to add header

---
+++ matmul2.h
@@ -0,0 +1,16 @@
+
+#include <stdint.h>
+
+#ifndef MATMUL2_H
+#define MATMUL2_H
+
+typdef struct _cache_line {
+  uint8_t v;
+  uint8_t m;
+  uint8_t index;
+  uint32_t data;
+  uint64_t tag; 
+} cache_line;
+
+
+#endif
